e7784994435968542f1f0a2ae11151bd602ec848
Hamza El Hilali <hamza.el.hilali505@gmail.com>
2026-04-22 13:59:12 -0400

    Added updateStock method - closes #1 - Hamza-El-Hilali

--- InventoryTracker.py
+++ InventoryTracker.py
@@ -4,3 +4,7 @@ class InventoryTracker:
     def addItem(self, name, quantity):
         """Add an item to the inventory"""
         print(f"Added {quantity} of {name} to inventory")
+
+    def updateStock(self, item_name, new_quantity):
+        """Update the quantity of an existing item in the inventory"""
+        print(f"Updated {item_name} quantity to {new_quantity}")
