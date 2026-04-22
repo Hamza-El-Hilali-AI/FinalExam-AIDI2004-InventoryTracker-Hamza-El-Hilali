a186230818025a575cda2caa9462cbb29c1304f6
Hamza El Hilali <hamza.el.hilali505@gmail.com>
2026-04-22 13:56:05 -0400

    Initial commit - Final Exam AIDI 2004 - Hamza-El-Hilali

---
+++ InventoryTracker.py
@@ -0,0 +1,6 @@
+# Final Exam AIDI 2004 - Hamza-El-Hilali
+
+class InventoryTracker:
+    def addItem(self, name, quantity):
+        """Add an item to the inventory"""
+        print(f"Added {quantity} of {name} to inventory")
